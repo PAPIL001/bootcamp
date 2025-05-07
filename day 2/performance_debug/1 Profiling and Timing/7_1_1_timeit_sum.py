9aef4ea118b632cef78abae506cf09ca7a81d502
PAPIL VERMA <55889535+PAPIL001@users.noreply.github.com>
2025-05-07 15:59:59 +0530

    section 7

--- day 2/performance_debug/1 Profiling and Timing/7_1_1_timeit_sum.py
+++
@@ -1,5 +0,0 @@
-
-import timeit
-
-execution_time = timeit.timeit('sum(range(10000))', number=1000)
-print(f"Execution time: {execution_time:.6f} seconds")  # Expected Output: Execution time: <some_time> seconds
